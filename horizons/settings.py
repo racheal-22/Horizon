ed450bde8a2c585fe075e633b1b15440448e557d
Racheal Pereira <pereiraracheal22@gmail.com>
2026-07-09 11:55:58 +0530

    Changes in settings ai  files

--- horizons/settings.py
+++ horizons/settings.py
@@ -202,3 +202,9 @@ CACHES = {
 }
 
 WSGI_APPLICATION = 'horizons.wsgi.application'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
